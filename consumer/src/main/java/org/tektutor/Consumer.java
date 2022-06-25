ad53bc372967042f033eda12fafb37b62a35f428
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-06-25 08:33:50 +0530

    Initial commit.

---
+++ consumer/src/main/java/org/tektutor/Consumer.java
@@ -0,0 +1,43 @@
+package org.tektutor;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.qpid.jms.JmsConnectionFactory;
+
+public class Consumer {
+
+   public static void main(String[] args) throws Exception {
+      Connection connection = null;
+      ConnectionFactory connectionFactory = new JmsConnectionFactory("amqp://localhost:5672");
+
+      try {
+         connection = connectionFactory.createConnection();
+
+         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+         connection.start();
+         Queue queue = session.createQueue("exampleQueue");
+         MessageConsumer consumer = session.createConsumer(queue);
+
+         TextMessage m = (TextMessage) consumer.receive(5000);
+         System.out.println("message = " + m.getText());
+
+         m = (TextMessage) consumer.receive(5000);
+         System.out.println("message = " + m.getText());
+
+         m = (TextMessage) consumer.receive(5000);
+         System.out.println("message = " + m.getText());
+
+      } finally {
+         if (connection != null) {
+            connection.close();
+         }
+      }
+   }
+}
---
+++ producer/src/main/java/org/tektutor/Producer.java
@@ -0,0 +1,44 @@
+package org.tektutor;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.qpid.jms.JmsConnectionFactory;
+
+public class Producer {
+
+   public static void main(String[] args) throws Exception {
+      Connection connection = null;
+      ConnectionFactory connectionFactory = new JmsConnectionFactory("amqp://localhost:5672");
+
+      try {
+
+         // Step 1. Create an amqp qpid 1.0 connection
+         connection = connectionFactory.createConnection();
+
+         // Step 2. Create a session
+         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+         // Step 3. Create a sender
+         Queue queue = session.createQueue("exampleQueue");
+         MessageProducer sender = session.createProducer(queue);
+
+         // Step 4. send a few simple message
+         sender.send(session.createTextMessage("Hello world 1 "));
+         sender.send(session.createTextMessage("Hello world 2"));
+         sender.send(session.createTextMessage("Hello world 3"));
+
+         connection.start();
+      } finally {
+         if (connection != null) {
+            // Step 9. close the connection
+            connection.close();
+         }
+      }
+   }
+}
